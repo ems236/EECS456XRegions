0ad89cfff2eb3e20c8e999deb4c06fb6e33cea3d
Ellis <ems236@case.edu>
2020-04-11 18:53:27 -0400

    added random region generation

--- src/region.py
+++ src/region.py
@@ -1,3 +1,7 @@
+from random import triangular, uniform
+
+MIN_SIDE_LENGTH = 2.0
+
 class Region:
     def __init__(self, x1, y1, x2, y2):
         self.x_min = min(x1, x2)
@@ -6,5 +10,10 @@ class Region:
         self.y_max = max(y1, y2)
 
     @staticmethod
-    def random_region(xcoord, ycoord):
-        pass
+    def random_region(xcoord, ycoord, max_size):
+        height = triangular(MIN_SIDE_LENGTH, max_size / MIN_SIDE_LENGTH)
+        width = max_size / height
+
+        xmin = xcoord - uniform(0, width)
+        ymin = ycoord - uniform(0, height)
+        return Region(xmin, ymin, xmin + width, ymin + height)
